5d117e7d2c66732a01acc52c8a10d4918c139540
Anne-Dienye <dienyeucheawaji93@gmail.com>
2025-02-19 14:07:58 +0000

    Add files via upload

---
+++ marks-2.py
@@ -0,0 +1,17 @@
+Enter a password: Password123!
+Valid: Password meets all requirements.
+
+Enter a password: pass
+Invalid: Password must be between 8 and 12 characters long.
+
+Enter a password: password123
+Invalid: Password must contain at least one uppercase letter.
+
+Enter a password: PASSWORD123
+Invalid: Password must contain at least one lowercase letter.
+
+Enter a password: Password!
+Invalid: Password must contain at least one digit.
+
+Enter a password: Password123
+Invalid: Password must contain at least one special character.
